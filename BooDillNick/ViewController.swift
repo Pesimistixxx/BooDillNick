0277de3033d6a75754a59c71f879249e2be39fbc
Pesimistixxx <musienkoiy@gmail.com>
2024-07-29 21:43:46 +0300

    Initial Commit

---
+++ BooDillNick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BooDillNick
+//
+//  Created by Компьютер on 29.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var HourImage: UIImageView!
+    @IBOutlet var MinuteImage: UIImageView!
+    @IBOutlet var SecondImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
